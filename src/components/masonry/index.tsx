57de59632f3f03393fa5819482ff0b1ff92beded
马强 <qiang.ma1@maqiangdeMacBook-Pro.local>
2026-09-04 10:04:21 +0800

    update blog

--- src/components/masonry/index.tsx
+++ src/components/masonry/index.tsx
@@ -60,7 +60,7 @@ const Wrong = (props: any) => (
 const Cry = (props: any) => (
   <IconButton
     {...props}
-    className="button"
+    className="button cry"
     size="1.5em"
     sx={{ p: 0, position: 'absolute', top: '25px', right: '5px' }}
   >
@@ -84,6 +84,38 @@ const Cry = (props: any) => (
           fill: `#f44336`
         }}
         p-id="3467"
+      ></path>
+    </svg>
+  </IconButton>
+)
+
+const Smile = (props: any) => (
+  <IconButton
+    {...props}
+    className="button smile"
+    size="1.5em"
+    sx={{ p: 0, position: 'absolute', top: '25px', right: '5px' }}
+  >
+    <svg
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+    >
+      <path
+        d="M512 74.666667C270.933333 74.666667 74.666667 270.933333 74.666667 512S270.933333 949.333333 512 949.333333 949.333333 753.066667 949.333333 512 753.066667 74.666667 512 74.666667z m0 810.666666c-204.8 0-373.333333-168.533333-373.333333-373.333333S307.2 138.666667 512 138.666667 885.333333 307.2 885.333333 512 716.8 885.333333 512 885.333333z"
+        sx={{
+          fill: `var(--theme-ui-colors-green-5)`
+        }}
+        p-id="1684"
+      ></path>
+      <path
+        d="M674.133333 608c-46.933333 57.6-100.266667 85.333333-162.133333 85.333333s-115.2-27.733333-162.133333-85.333333c-10.666667-12.8-32-14.933333-44.8-4.266667-12.8 10.666667-14.933333 32-4.266667 44.8 59.733333 70.4 130.133333 106.666667 211.2 106.666667s151.466667-36.266667 211.2-106.666667c10.666667-12.8 8.533333-34.133333-4.266667-44.8-12.8-10.666667-34.133333-8.533333-44.8 4.266667zM362.666667 512c23.466667 0 42.666667-19.2 42.666666-42.666667v-64c0-23.466667-19.2-42.666667-42.666666-42.666666s-42.666667 19.2-42.666667 42.666666v64c0 23.466667 19.2 42.666667 42.666667 42.666667zM661.333333 512c23.466667 0 42.666667-19.2 42.666667-42.666667v-64c0-23.466667-19.2-42.666667-42.666667-42.666666s-42.666667 19.2-42.666666 42.666666v64c0 23.466667 19.2 42.666667 42.666666 42.666667z"
+        sx={{
+          fill: `var(--theme-ui-colors-green-5)`
+        }}
+        p-id="1685"
       ></path>
     </svg>
   </IconButton>
@@ -147,8 +179,8 @@ export const MasonryItem = ({ title, children, index }: MasonryItemProps) => {
       masonryItemState[index] === undefined
         ? normalColor
         : masonryItemState[index]
-        ? successColor
-        : failureColor,
+          ? successColor
+          : failureColor,
     [masonryItemState[index]]
   )
 
@@ -206,7 +238,10 @@ export const MasonryItem = ({ title, children, index }: MasonryItemProps) => {
         >
           {title}
           {globalStudySwitch === true && contentColor === successColor && (
-            <Cry onClick={() => reMarkFailed()} />
+            <div className="masonry-item-mark" onClick={() => reMarkFailed()}>
+              <Cry />
+              <Smile />
+            </div>
           )}
         </div>
       )}
